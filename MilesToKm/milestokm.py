76792466a6ce3a8d1de721cd78147580860107c6
Sampson <siezieme@ucdavis.edu>
2023-02-19 00:36:13 -0800

    Convert miles to kilometers

---
+++ MilesToKm/milestokm.py
@@ -0,0 +1,13 @@
+## milestokm.py
+## all of us
+## October 3, 2016
+##
+## Convert miles to kilometers
+
+miles = float(input("Please enter miles: "))
+
+km_per_mile = 1.6
+
+kilometers = miles * km_per_mile
+
+print(miles, "miles are", kilometers, "km")
